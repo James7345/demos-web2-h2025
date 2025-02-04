aa95d31110bd670b6a1f6051e3ae732111449c36
James7345 <jamesbourassaarvisais@gmail.com>
2025-02-04 09:44:36 -0500

    e1

--- cours03/E1.js
+++ cours03/E1.js
@@ -8,9 +8,15 @@ class tabProduits {
         console.log(` ${this.nom}, ${this.prix} $.`);
     }
 }
-const Produit1 = new Produit("Ordinateur", 1200);
-const Produit2 = new Produit("Clavier", 100);
-const Produit3 = new Produit("Moniteur", 400);
-const Produit4 = new Produit("Souris", 50);
+const Produit1 = new tabProduits("Ordinateur", 1200);
+const Produit2 = new tabProduits("Clavier", 100);
+const Produit3 = new tabProduits("Moniteur", 400);
+const Produit4 = new tabProduits("Souris", 50);
 
-console.log(tabProduits);
+const produits = [Produit1, Produit2, Produit3, Produit4];
+
+for(const tabProduits of produits) {
+    tabProduits.afficherDetails();
+}
+
+console.log(produits);
